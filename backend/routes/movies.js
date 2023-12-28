4465b73fdefeca10875c82e5a25dd60cbecc83a8
Rafi <rafal.kowalski@student.hogent.be>
2023-12-28 22:55:05 +0100

    add prisma WIP

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const movies = require("../data/movies.json");
-const logger = require("morgan");
+// const logger = require("morgan");
+// const mysql = require("mysql2");
+const prisma = require("../config/prisma_db");
 
-router.get("/", function (req, res, next) {
-    res.send(movies);
+router.get("/", async (req, res, next) => {
+  try {
+    const films = await prisma.film.findMany();
+    res.send(
+      films.map((m) => {
+        return {
+          title: m.titel,
+          year: m.releasejaar,
+          genres: [m.genre],
+          extract: m.beschrijving,
+          thumbnail: m.image,
+        };
+      })
+    );
+  } catch (error) {
+    res.status(500).send("Er is een fout opgetreden");
+  }
 });
 
-router.get("/:id", function (req, res, next) {
-    const { params: { id } } = req;
-    res.send(movies[id]);
+
+router.get("/:id+", async function (req, res, next) {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const m = await prisma.film.findFirst({ where: {id: +id} });
+    res.send({
+      title: m.titel,
+      year: m.releasejaar,
+      genres: [m.genre],
+      extract: m.beschrijving,
+      thumbnail: m.image,
+      cast: []
+    });
+  } catch (error) {
+    res.status(500).send("Er is een fout opgetreden");
+  }
 });
 
 module.exports = router;
